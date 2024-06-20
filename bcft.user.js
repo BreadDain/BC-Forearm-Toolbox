3dfd6f9f83a903b61f2ddc8b714f9b53e17c748e
BreadDain <Virus1_@mail.ru>
2024-06-20 14:46:24 +0300

    Create bcft.user.js

---
+++ bcft.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         Siena's forearm toolbox
+// @namespace    https://www.bondageprojects.com/
+// @version      1.0
+// @description  You know what is this addon for.
+// @author       Siena
+// @match https://bondageprojects.elementfx.com/*
+// @match https://www.bondageprojects.elementfx.com/*
+// @match https://bondage-europe.com/*
+// @match https://www.bondage-europe.com/*
+// @match http://localhost:*/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=bondage-europe.com
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    "use strict";
+    
+    javascript:void function(){function httpGet(a){var b=new XMLHttpRequest;return b.open("GET",a,!1),b.send(null),b.responseText}var code=httpGet("https://raw.githubusercontent.com/BreadDain/BC-Forearm-Toolbox/new/main/bcft.js");const script=document.createElement("script");script.type="text/javascript",script.innerHTML=code,document.head.appendChild(script),eval(script)}();
+})();
