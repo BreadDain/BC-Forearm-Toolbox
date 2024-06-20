69955581dc44a338ef9861509f6c689d6902c4c9
BreadDain <Virus1_@mail.ru>
2024-06-20 14:52:26 +0300

    Update bcft.user.js

--- bcft.user.js
+++ bcft.user.js
@@ -16,5 +16,5 @@
 (function () {
     "use strict";
     
-    javascript:void function(){function httpGet(a){var b=new XMLHttpRequest;return b.open("GET",a,!1),b.send(null),b.responseText}var code=httpGet("https://raw.githubusercontent.com/BreadDain/BC-Forearm-Toolbox/new/main/bcft.js");const script=document.createElement("script");script.type="text/javascript",script.innerHTML=code,document.head.appendChild(script),eval(script)}();
+    javascript:void function(){function httpGet(a){var b=new XMLHttpRequest;return b.open("GET",a,!1),b.send(null),b.responseText}var code=httpGet("https://raw.githubusercontent.com/BreadDain/BC-Forearm-Toolbox/main/bcft.js");const script=document.createElement("script");script.type="text/javascript",script.innerHTML=code,document.head.appendChild(script),eval(script)}();
 })();
