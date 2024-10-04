3ebe59c3b9895ffde54497a02ebca1bf959ca52a
BreadDain <Virus1_@mail.ru>
2024-10-04 22:44:44 +0300

    Update bcft.user.js

--- bcft.user.js
+++ bcft.user.js
@@ -3,12 +3,13 @@
 // @namespace    https://www.bondageprojects.com/
 // @version      1.0
 // @description  No Description
-// @author       Siena
+// @author       Lorfem
 // @match https://bondageprojects.elementfx.com/*
-// @match https://www.bondageprojects.com/
 // @match https://www.bondageprojects.elementfx.com/*
 // @match https://bondage-europe.com/*
 // @match https://www.bondage-europe.com/*
+// @match https://bondageprojects.com/*
+// @match https://www.bondageprojects.com/*
 // @match http://localhost:*/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
